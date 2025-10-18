871ad2c67f5d8c08d661d9ed84d15786fa6fb240
hitlbungee09 <238786330+hitlbungee09@users.noreply.github.com>
2025-10-17 21:11:58 -0500

    Update minuteFarm.js

--- functions/minuteFarm.js
+++ functions/minuteFarm.js
@@ -6,7 +6,7 @@ window.fetch = async function (input, init = {}) {
     else if (init.body) body = init.body;
     if (features.minuteFarmer && body && input.url.includes("mark_conversions")) {
         try {
-            if (body.includes("termination_event")) { sendToast("🚫 Limitador de tempo bloqueado.", 1000); return; }
+            if (body.includes("termination_event")) { sendToast("🚫 no puedo farmear mas tiempo sorry.", 1000); return; }
         } catch (e) { debug(`🚨 Error @ minuteFarm.js\n${e}`); }
     }
     return originalFetch.apply(this, arguments);
