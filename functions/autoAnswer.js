4726cb8b7fcbf633e14cdb796a900564df19323c
hitlbungee09 <238786330+hitlbungee09@users.noreply.github.com>
2025-10-17 21:19:25 -0500

    Update autoAnswer.js

--- functions/autoAnswer.js
+++ functions/autoAnswer.js
@@ -6,25 +6,32 @@ const baseSelectors = [
     `._awve9b`
 ];
 
-khanwareDominates = true;
+let khanwareDominates = true;
 
 (async () => { 
+    sendToast("🤖 Voy a responder de forma automatica, si lo haces tu en instantaneo van a sospechar, cuidado con el descaro :v", 7000);
+
     while (khanwareDominates) {
         if (features.autoAnswer && features.questionSpoof) {
             
             const selectorsToCheck = [...baseSelectors];
-
-            if (features.nextRecomendation) baseSelectors.push("._hxicrxf")
-            if (features.repeatQuestion) baseSelectors.push("._ypgawqo");
+            if (features.nextRecomendation) selectorsToCheck.push("._hxicrxf");
+            if (features.repeatQuestion) selectorsToCheck.push("._ypgawqo");
 
             for (const q of selectorsToCheck) {
                 findAndClickBySelector(q);
-                if (document.querySelector(q+"> div") && document.querySelector(q+"> div").innerText === "Mostrar como lo hice entre muchas comillas") {
-                    sendToast("🎉 Ya termine!", 3000);
+                if (document.querySelector(q + "> div") && document.querySelector(q + "> div").innerText === "Mostrar como lo hice entre muchas comillas") {
+                    sendToast("🎉 Ya termine", 3000);
                     playAudio("https://r2.e-z.host/4d0a0bea-60f8-44d6-9e74-3032a64a9f32/4x5g14gj.wav");
                 }
             }
         }
-        await delay(featureConfigs.autoAnswerDelay * 800);
+        
+        const minDelay = 30000;
+        const maxDelay = 60000; 
+
+        const randomDelay = Math.floor(Math.random() * (maxDelay - minDelay + 1)) + minDelay;
+        
+        await delay(randomDelay);
     }
 })();
