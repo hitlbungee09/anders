c6d23a570815550c9fb03ede3c3d51af92385215
hitlbungee09 <238786330+hitlbungee09@users.noreply.github.com>
2025-10-17 21:13:57 -0500

    Update videoSpoof.js

--- functions/videoSpoof.js
+++ functions/videoSpoof.js
@@ -14,7 +14,7 @@ window.fetch = async function (input, init) {
                 body = JSON.stringify(bodyObj);
                 if (input instanceof Request) { input = new Request(input, { body: body }); } 
                 else init.body = body; 
-                sendToast("🔓 Vídeo exploitado.", 1000)
+                sendToast("🔓 Me vi el video por ti.", 1000)
             }
         } catch (e) { debug(`🚨 Error @ videoSpoof.js\n${e}`); }
     }
